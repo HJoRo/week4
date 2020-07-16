a5655e8ba17f932448de3cc948fbd20f706405d1
Haley <h.rohe96@gmail.com>
2020-07-16 19:23:48 -0400

    conditional statement

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-var firstVar = "5";
-var secondVar = 3;
+//var firstVar = "5";
+//var secondVar = 3;
 
-console.log(firstVar * secondVar);
-console.log(2 + true);
+//console.log(firstVar * secondVar);
+//console.log(2 + true);
+
+var count = (2 + 5) * 2;
+//2
+console.log(count % 3);
+
+var x = 7;
+if(x % 2 == 0){
+    console.log('even')
+}else {
+    console.log('odd')
+};
+
+var notBored = true;
+var homework = true;
+if (notBored !== true){
+    console.log("i'm bored");
+}else{
+    if (homework !== true){
+    console.log("do homework!");
+}else{
+    if (notBored == true && homework == true){
+        console.log("watch a movie!");
+    }
+}
+};
